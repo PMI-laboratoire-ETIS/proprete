0c5593e178b95fce96ab64ad7ef32c8048e49c6e
unknown <victor.du-crest@ipsa.fr>
2019-11-06 16:12:31 +0100

    Avec ImageDataGenerator et bruit normal

--- modele_grele.py
+++ modele_grele.py
@@ -1,51 +1,52 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Oct 12 12:20:21 2019
+Created on Tue Oct 29 11:34:06 2019
 
-@author: julien
+@author: Victor du Crest
 """
 
-#%% imports
-import numpy as np
-import pandas as pd
-
-import keras
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
 from keras.preprocessing.image import ImageDataGenerator
 from keras.regularizers import l2
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import recall_score, roc_curve
+import keras
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
 
-import matplotlib.pyplot as plt
+imdir = "images_intestins"
 
-import os
-from PIL import Image
+def bruit(image, λ):
+    img = image + np.random.normal(size=image.shape) * image * λ
+    img[img < 0] = 0 ; img[img > 255] = 255
+    return img
 
-batch_size = 32
-epochs = 100
-augimdir = "BDD/train/images_intestins_augmentées"
-testimdir = "BDD/test"
 
-#%% Load & crop
-x_train, y_train = zip(*((np.asarray(Image.open(os.path.join(augimdir,fname)).resize((150,150))), 1 if "propre" in fname else 0) for fname in os.listdir(augimdir) if fname[-4:] == '.png'))
 
-x_train = np.asarray(x_train) / 255
-y_train = np.asarray(y_train) / 255
+datagen = ImageDataGenerator(
+        rescale=1./255,
+        horizontal_flip=True,
+        vertical_flip=True,
+        fill_mode='constant',
+        cval = 0,
+        validation_split=1/6,
+        preprocessing_function=lambda img : bruit(img, 0.1)) #Modèle du générateur automatique
 
-input_shape = x_train.shape[1:]
+input_shape = (512, 512, 3) 
 
-test_data = ImageDataGenerator(rescale=1/255).flow_from_directory(
-        testimdir,
-        target_size=input_shape[:-1],
-        batch_size=100,
-        class_mode="binary"
-        )
-x_test, y_test = next(test_data)
-from sklearn.preprocessing import scale
-scale
+train_generator = datagen.flow_from_directory(
+        imdir,
+        target_size=input_shape[:2],
+        batch_size=32,
+        class_mode='binary',
+        classes=['propre', 'sale'],
+        subset='training')
 
-#%% Network creation
+validation_generator = datagen.flow_from_directory(
+        imdir,
+        target_size=input_shape[:2],
+        batch_size=32,
+        classes=['propre', 'sale'],
+        class_mode='binary',
+        subset='validation')
 
 model = Sequential()
 
@@ -72,49 +73,11 @@ model.compile(loss=keras.losses.binary_crossentropy,
               optimizer="adam",
               metrics=['accuracy'])
 
-#%% Learn data
-history = model.fit(x_train, y_train,
-                    
-          batch_size=batch_size,
-          epochs=epochs,
-          verbose=1,
-          validation_data=(x_test, y_test))
 
-#%% Plot history of learning process
-figm, axs = plt.subplots(1, 2, clear=True, num="Metrics")
-axs[0].semilogy(history.history["loss"], label="loss")
-axs[0].semilogy(history.history["val_loss"], label="val_loss")
-axs[0].legend()
-axs[0].set_xlim(0)
-axs[0].set_title("Loss evolution (semilogy)")
-axs[0].set_xlabel("Epoch")
-axs[0].set_ylabel("Loss (binary cross-entropy)")
-axs[1].plot(history.history["acc"], label="acc")
-axs[1].plot(history.history["val_acc"], label="val_acc")
-axs[1].legend()
-axs[1].set_xlim(0)
-axs[1].set_ylim(0,1)
-axs[1].set_title("Accuracy evolution")
-axs[1].set_xlabel("Epoch")
-axs[1].set_ylabel("Accuracy")
 
-#%% Check accuracy of model
-y_pred = model.predict(x_test)
-y_pred_classes = model.predict_classes(x_test)
-
-sensitivity = recall_score(y_test, y_pred_classes)
-specificity = recall_score(1-y_test, 1-y_pred_classes)
-
-print("Sensitivity: {:.2%}".format(sensitivity))
-print("Specificity: {:.2%}".format(specificity))
-
-fpr, tpr = roc_curve(y_test, y_pred)[:-1]
-
-figroc, axroc = plt.subplots(1, 1, clear=True, num="Courbe ROC")
-axroc.plot(fpr, tpr, label="CNN")
-axroc.plot([0,1],[0,1],'r--')
-axroc.set_xlim(0,1)
-axroc.set_ylim(0,1)
-axroc.set_title("Courbe ROC", fontsize=15)
-axroc.scatter(1 - specificity, sensitivity, marker="d", label="Point d'opération")
-axroc.legend()
+model.fit_generator(
+        train_generator,
+        steps_per_epoch=2000,
+        epochs=50,
+        validation_data=validation_generator,
+        validation_steps=800)
